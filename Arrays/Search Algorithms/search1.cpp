8d2f8c77808a912e941928257f26fbbc69a43dda
Satchidanandan <satchidanandan@student.tce.edu>
2024-08-13 15:02:02 +0530

    SEARCH ALGOS

---
+++ Arrays/Search Algorithms/search1.cpp
@@ -0,0 +1,20 @@
+//C++ Program to count the number of unique elements in an array of n elements.
+#include<iostream>
+using namespace std;
+int main()
+{
+int arr[10],n=5;
+cout<<"Enter the 5 elements: ";
+for(int i=0;i<n;i++)
+{cin>>arr[i];}
+int un=1;
+for(int i=1;i<n;i++)
+{ int j=0;
+for(j=0;j<i;j++)
+if(arr[i]==arr[j])
+break;
+if(i==j)
+un++; }
+cout<<"unique elements are "<<un;
+return 0;
+}
